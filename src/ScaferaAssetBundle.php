46036c8f237781f3fad128a3ecab0f0bf981583d
samaphp <samaphp@users.noreply.github.com>
2026-04-14 16:45:34 +0300

    Resolve asset mapper path from architecture package instead of hardcoding

--- src/ScaferaAssetBundle.php
+++ src/ScaferaAssetBundle.php
@@ -4,6 +4,7 @@ declare(strict_types=1);
 
 namespace Scafera\Asset;
 
+use Scafera\Kernel\InstalledPackages;
 use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
@@ -12,9 +13,17 @@ final class ScaferaAssetBundle extends AbstractBundle
 {
     public function prependExtension(ContainerConfigurator $container, ContainerBuilder $builder): void
     {
+        $projectDir = $builder->getParameter('kernel.project_dir');
+        $architecture = InstalledPackages::resolveArchitecture($projectDir);
+        $assetsDir = $architecture?->getAssetsDir();
+
+        if ($assetsDir === null) {
+            return;
+        }
+
         $builder->prependExtensionConfig('framework', [
             'asset_mapper' => [
-                'paths' => ['assets/'],
+                'paths' => [$assetsDir . '/'],
             ],
         ]);
     }
